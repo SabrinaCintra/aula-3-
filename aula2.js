0e3ba74282ec4f490bdf1ba90283d683e9bbef5f
SabrinaCintra <sabrinacintra234@gmail.com>
2021-09-13 14:52:48 -0300

    first commit

---
+++ aula2.js
@@ -0,0 +1,30 @@
+ 
+ 
+ 
+ 
+ var nome_do_aluno = "Gabriel"
+ var nota1 = 8 
+ var nota2 = 7
+ var nota3 = 9
+ var nota4 = 10
+ var media = (nota1 + nota2 + nota3 + nota4)/4;
+
+
+
+console.log("nome do aluno, " + nome_do_aluno);
+console.log("Sua média é: " + media);
+
+ 
+
+ 
+ if (media > 6){
+     console.log("Voce foi aprovado");
+ }
+
+ else if (media == 6){
+     console.log("Voce esta de recuperacao");
+ }
+
+ else {
+     console.log("Voce foi reprovado");
+ }
